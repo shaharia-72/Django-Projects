fb9a262f10f699df64d6a1532959fdd8c1ca4927
Shaharia <shahariahossain72@gmail.com>
2024-08-23 15:28:40 +0600

    b_updates

---
+++ bank_management/accounts/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class BankAccountPersonal(models.Model):
+    user = models.OneToOneField(
+        User, related_name='account', on_delete=models.CASCADE)
